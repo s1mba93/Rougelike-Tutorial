9d79e6a0609df2a8237ec282e961270a6c76b73f
s1mba93 <danielelson93@gmail.com>
2024-02-03 21:34:19 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import tcod
+import os
+import sys
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+os.environ["path"] = os.path.dirname(sys.executable) + ";" + os.environ["path"]
+
+
+DATA_FOLDER = "data"
+FONT_FILE = os.path.join(DATA_FOLDER, "dejavu10x10_gs_tc.png")
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        FONT_FILE, 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_width,
+        tileset=tileset,
+        title="Chaos Forge Roguelike",
+        vsync=True,
+    ) as context:
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
